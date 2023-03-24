93a0583fbccdd9243f022d3848169a57f6371b07
khute <khute@BibiKhuteja>
2023-03-25 03:01:39 +0530

    data form

--- src/main/java/ResumeCon.java
+++ src/main/java/ResumeCon.java
@@ -1,4 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-public class FirstJava {
+import com.mysql.cj.jdbc.Driver;
 
+public class ResumeCon {
+  
+	private static final String url="jdbc:mysql://localhost:3306/resume_generator";
+	private static final String user="root";
+	private static final String pass="root";
+	public static Connection makeConnection() throws Exception{
+		Connection con;
+		Driver d=new com.mysql.cj.jdbc.Driver();
+		DriverManager.registerDriver(d);
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con=DriverManager.getConnection(url,user,pass);
+		return con;
+	}
+	public static Connection closeConnection() throws Exception{
+		Connection con=makeConnection();
+		con.close();
+		return con;
+	}
 }
+
